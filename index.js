284ac8354d22aa8b8e7990e4961b9784639e6fb5
StormKip <storm.kippie@younglings.africa>
2019-05-22 08:18:43 +0200

    Upload Project

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import a Library to help create a component
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a component
+
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <Header headerText="Albums!" />
+        <AlbumList />
+    </View>
+);
+
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
